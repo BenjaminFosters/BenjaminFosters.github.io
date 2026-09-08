3fda35f97412c8ff1f3c9b034f2ec996e086f84f
BenjaminFosters <ArthurMacTavish@outlook.com>
2026-09-08 12:22:40 +0700

    Putting Contact Info into a file + Footer Copyright Responsive Change
    Putting Contact info into separate file + Changing Footer Copyright/Licensing Information Position between desktop & mobile.

---
+++ src/data/contacts.ts
@@ -0,0 +1,15 @@
+export interface Contact {
+  name: string;
+  href: string;
+}
+
+export const contacts: Contact[] = [
+  {
+    name: "Email Inbox",
+    href: "mailto:inbox@reubenhu.my.id",
+  },
+  {
+    name: "Matrix @ Beeper",
+    href: "https://matrix.to/#/@reubenhu:beeper.com",
+  },
+];
